da20c17a172233f160e41f4a82562e43c15390e1
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-26 21:04:03 -0400

    classes Produto e Categoria

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,7 +1,28 @@
 ﻿namespace Linq {
-    internal class Program {
+    class Program 
+    {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
         }
     }
+
+
+    class Produto 
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public bool Status { get; set; }    
+        public decimal Valor { get; set; }  
+        public int CategoriaId { get; set; }
+
+    }
+
+    class Categoria {
+
+        public int id  { get; set; }    
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+
+    }
+
 }
